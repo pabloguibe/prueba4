2c15fc0bf96f9b16f62a6fb70fa67facf728b229
pablo guisasola <pabloguibe01@gmail.com>
2022-10-03 19:38:39 +0200

    formularioJS

--- js/myscript.js
+++ js/myscript.js
@@ -43,6 +43,8 @@ let surname3= "Doe";
 let question3= "How are you ".concat(name3)+" ".concat(surname3)+"?";
 console.log(question);
 
+console.log("------------------------")
+
 //Arithmetic operations
 //Sum,Rest...
 
@@ -57,6 +59,8 @@ console.log(rest);
 console.log(mult);
 console.log(division);
 
+console.log("------------------------")
+
 //Arithmetic operations II
 //Exponentiation / Increment / Decrement
 
@@ -68,6 +72,8 @@ console.log(++operador_c);
 console.log(operador_d--);
 console.log(operador_d);
 
+console.log("------------------------")
+
 //Assignment Operators
 
 let x= 5;
@@ -76,6 +82,8 @@ console.log(x+=5);
 console.log(x-=5);
 console.log(y*=3);
 console.log(y/=2);
+
+console.log("------------------------")
 
 //typeof, null y undefinded
 
@@ -94,6 +102,8 @@ console.log(typeof(testNull));
 let testUndefined;
 console.log(testUndefined);
 
+console.log("------------------------")
+
 //Array
 
 var first_array=[];
@@ -105,14 +115,20 @@ console.log(second_array);
 console.log(third_array);
 console.log(fourth_array);
 
+console.log("------------------------")
+
 //Array acces
 
 console.log(third_array[1]);
 console.log(fourth_array[2][0]);
 
+console.log("------------------------")
+
 //Array length
 
 console.log(fourth_array.length);
+
+console.log("------------------------")
 
 //Array push
 
@@ -121,10 +137,14 @@ console.log(fourth_array.push("Spain"));
 fourth_array.push("Spain");
 console.log(fourth_array);
 
+console.log("------------------------")
+
 //Array unshift
 
 fourth_array.unshift(1);
 console.log(fourth_array);
+
+console.log("------------------------")
 
 //For / For Each
 
@@ -147,3 +167,199 @@ fourth_array.forEach(function(element){
     console.log("Entramos en la iteracion de "+fourth_array[i])
 });
 
+console.log("------------------------")
+
+//If / else/ elseif
+
+let aux=3;
+if (aux>0){
+    console.log("This number is positive");
+}else if(aux <0){
+    console.log("This number is negative");
+
+}else{
+console.log ("This number is 0");
+
+}
+
+console.log("------------------------")
+// Regular Expressions
+
+var DNI_REGEX = /^(\d{8})([A-Z])$/;
+let dni="12345678K";
+if(dni.match(DNI_REGEX)){
+    console.log("Correct DNI");
+}else{
+    console.log("Incorrect DNI");
+}
+
+console.log("------------------------")
+// Compasrison Operators
+console.log(8==8);
+console.log(8=="8");
+console.log(8===8);
+console.log(8==="8");
+console.log("------------------------")
+console.log("test"=="test")
+console.log("test"==='test')
+console.log("test"=="Test")
+
+console.log("------------------------")
+
+//Loop while
+
+var i=1;
+
+while(i<10){
+    console.log("The number is " + i);
+    i++;
+}
+
+console.log("------------------------")
+
+//Date Object
+
+let today = new Date();
+let first_october= new Date(2019, 10, 1);
+console.log(today);
+console.log(first_october);
+console.log(today.getDay());
+
+if(today>first_october){
+    console.log("Today is after to fist octuber");
+
+}else{
+    console.log("Today is before to first octuber");
+}
+
+console.log("------------------------")
+
+//Eventos
+
+function myFirstFunction(){
+    console.log("Thank you for you click")
+}
+
+function mySecondFunction(){
+    console.log("Thank you for you interest.")
+}
+
+console.log("------------------------")
+
+
+//Select DOM
+
+var div= document.getElementById("my_div");
+div.classList.add("my_class");
+console.log(div)
+
+console.log("------------------------")
+
+var div2= document.getElementsByTagName("div");
+console.log(div2);
+
+console.log("------------------------")
+
+var second_div= document.querySelector("#my_second_div");
+console.log(second_div);
+
+
+console.log("------------------------")
+
+/*function $(selector) {
+    return document.querySelectorAll(selector);
+
+}
+*/
+//console.log($("#my_third_div"));
+//console.log($(".div_especial"));
+
+console.log("------------------------")
+
+//Funciones
+
+var numbers =[1,2,3,4];
+var n_elevator_2= numbers.map(function(n){ return n*n; });
+console.log(n_elevator_2);
+
+console.log("------------------------")
+
+var n_elevator_2= numbers.map((n) => {return n*n; });
+console.log(n_elevator_2);
+
+console.log("------------------------")
+
+var n_elevator_2 =numbers.map ((n) => { console.log(n*n); return n*n;});
+console.log(n_elevator_2);
+
+console.log("------------------------")
+
+var n_elevator_2=numbers.map(n =>n*n);
+console.log(n_elevator_2);
+
+console.log("------------------------")
+
+let numbers5 = [[1,1],[2,2],[3,3],[4,4]];
+let itself = numbers5.map(([x,y]) => x*y);
+console.log(itself);
+
+
+
+console.log("------------------------")
+
+//Create Dom Nodes I
+
+/*$("#btn2").addEventListener("click",function(){
+    var input = document.createElement("input");
+    console.log(input);
+
+});
+*/
+/*function $(selector) {
+    return document.querySelector(selector);
+
+}
+*/
+
+console.log("------------------------")
+
+//Create Dom Nodes 
+
+$("#btn2").addEventListener("click",function(){
+    var input= document.createElement("input");
+    input.setAttribute("type","email");
+    input.setAttribute("placeholder","E-mail");
+    input.setAttribute("name","emails[]");
+    $("#form").appendChild(input);
+    myAlert("Add new email input");
+});   
+
+function $(selector) {
+    return document.querySelector(selector);
+
+}
+
+function myAlert(msg) {
+    var div=document.createElement("div");
+    div.classList.add("alert");
+    div.innerHTML= msg;
+    var close=document.createElement("span");
+    close.style.float="right";
+    close.classList.add("close");
+    close.innerHTML= "X";
+    div.appendChild(close);
+    $("body").insertBefore(div,$("body").firstChild);
+    bind_close();
+
+
+}
+
+function bind_close(){
+    let elements= document.querySelectorAll(".close");
+    for(var i=elements.length-1;i>=0;i--){
+        let el = elements [i];
+        el.addEventListener("click", function(){
+            this.parentNode.style.display = "none";
+        });
+    }
+}
